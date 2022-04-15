eb880b1556412be0c213e45cd1ab97cef39415b7
Vlad Konovalchuk <vkonovalchuk@lohika.com>
2022-04-15 11:48:50 +0300

    fix directory path

--- ssg/site.py
+++ ssg/site.py
@@ -7,8 +7,8 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = Path(f"{self.dest}/{path.relative_to(self.source)}")
-        directory.mkdir(parent=True, exist_ok=True)
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
